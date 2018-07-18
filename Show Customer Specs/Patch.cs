5b1ea000fec8a9820cedc4f1ba85be255246a37f
synogen <synogen@freenet.de>
2018-07-18 02:49:30 +0200

    customer specs mod adapted for harmony

---
+++ Show Customer Specs/Patch.cs
@@ -0,0 +1,39 @@
+﻿using Harmony;
+
+namespace Show_Customer_Specs
+{
+    [HarmonyPatch(typeof(Job))]
+    [HarmonyPatch("GetBody")]
+    class Patch
+    {
+        static string Postfix(string __result, Job __instance)
+        {
+            string specs = "";
+            if (
+                __instance.GetJobType() == JobDesc.Type.DIAGNOSE ||
+                __instance.GetJobType() == JobDesc.Type.FIX ||
+                __instance.GetJobType() == JobDesc.Type.UPGRADE
+            )
+            {
+                specs = "\n\n<b>Current specs:</b>\n";
+                foreach (PartInstance part in __instance.GetComputer().GetAllParts())
+                {
+                    switch (part.GetPart().m_type)
+                    {
+                        case PartDesc.Type.MOTHERBOARD:
+                        case PartDesc.Type.GPU:
+                        case PartDesc.Type.HDD:
+                        case PartDesc.Type.RAM:
+                        case PartDesc.Type.CPU:
+                        case PartDesc.Type.SSD:
+                        case PartDesc.Type.PSU:
+                            string type = part.GetPart().m_type == PartDesc.Type.MOTHERBOARD ? "MB" : part.GetPart().m_type.ToString();
+                            specs = specs + type + ":  \t" + part.GetPart().m_uiName + "\n";
+                            break;
+                    }
+                }
+            }
+            return __result + specs;
+        }
+    }
+}
