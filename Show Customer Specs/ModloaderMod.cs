907814948c77e1dcc450f1bfce6d3f4aa8514f21
synogen <synogen@freenet.de>
2018-07-23 04:01:36 +0200

    added modloader file for Customer Specs mod

---
+++ Show Customer Specs/ModloaderMod.cs
@@ -0,0 +1,24 @@
+﻿using PCBSModloader;
+
+namespace Show_Customer_Specs
+{
+    class ModloaderMod : Mod
+    {
+        public override string ID { get { return "Show Customer Specs"; } }
+
+        public override string Version { get { return "1"; } }
+
+        public override string Author { get { return "synogen"; } }
+
+        private static ModloaderMod singletonInstance;
+
+        public static ModloaderMod Instance {
+            get
+            {
+                singletonInstance = singletonInstance != null ? singletonInstance : new ModloaderMod();
+                return singletonInstance;
+            }
+        }
+
+    }
+}
