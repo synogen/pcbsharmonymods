ae6bf654be6c8ea810c2397f9fa3241b63bd8c09
synogen <synogen@freenet.de>
2018-07-26 01:31:27 +0200

    Added working image replacer (works both on static and dynamic images!)  and updated readme

--- Texture Replacer/Patch.cs
+++ Texture Replacer/Patch.cs
@@ -1,32 +1,73 @@
 ﻿using Harmony;
-using System;
 using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 namespace Texture_Replacer
 {
+    class ConfigHolder
+    {
+        private static ConfigHolder configHolder;
+
+        public static ConfigHolder Instance
+        {
+            get
+            {
+                if (configHolder == null)
+                {
+                    configHolder = new ConfigHolder();
+                }
+                return configHolder;
+            }
+        }
+
+        public Dictionary<string, WWW> textureReplacements = new Dictionary<string, WWW>();
+
+        public Dictionary<string, WWW> imageReplacements = new Dictionary<string, WWW>();
+
+        private ConfigHolder()
+        {
+            File.Delete(ModloaderMod.Instance.Modpath + "/replacer.log");
+
+            string[] textureConfigurations = File.ReadAllLines(ModloaderMod.Instance.Modpath + "/Parts Texture Replacer.conf");
+            foreach (string textureConfigLine in textureConfigurations)
+            {
+                string[] textureConfig = textureConfigLine.Split('|');
+                if (textureConfig.Length == 2)
+                {
+                    textureReplacements.Add(textureConfig[0], new WWW(("file:///" + ModloaderMod.Instance.Modpath + "/" + textureConfig[1]).Replace(" ", "%20").Replace("\\", "/")));
+                    File.AppendAllText(ModloaderMod.Instance.Modpath + "/replacer.log", "Added replacement configuration for " + textureConfig[0] + " => " + textureConfig[1] + "\n");
+                }
+            }
+
+            string[] imageConfigurations = File.ReadAllLines(ModloaderMod.Instance.Modpath + "/Image Replacer.conf");
+            foreach (string imageConfigLine in imageConfigurations)
+            {
+                string[] imageConfig = imageConfigLine.Split('|');
+                if (imageConfig.Length == 2)
+                {
+                    imageReplacements.Add(imageConfig[0], new WWW(("file:///" + ModloaderMod.Instance.Modpath + "/" + imageConfig[1]).Replace(" ", "%20").Replace("\\", "/")));
+                    File.AppendAllText(ModloaderMod.Instance.Modpath + "/replacer.log", "Added replacement configuration for " + imageConfig[0] + " => " + imageConfig[1] + "\n");
+                }
+            }
+        }
+    }
+
     [HarmonyPatch(typeof(PartsDatabase))]
     [HarmonyPatch("InstantiatePart")]
     class PatchPartTextures
     {
         static GameObject Postfix(GameObject __result)
         {
-            string[] textureConfigurations = File.ReadAllLines(ModloaderMod.Instance.Modpath + "/Parts Texture Replacer.conf");
             MeshRenderer[] meshRenderers = __result.GetComponentsInChildren<MeshRenderer>(true);
             foreach (MeshRenderer meshRenderer in meshRenderers)
             {
                 if (meshRenderer.material.mainTexture != null && typeof(Texture2D).IsInstanceOfType(meshRenderer.material.mainTexture))
                 {
-                    foreach (string textureConfigLine in textureConfigurations)
+                    if (ConfigHolder.Instance.textureReplacements.ContainsKey(meshRenderer.material.mainTexture.name))
                     {
-                        string[] textureConfig = textureConfigLine.Split('|');
-                        if (meshRenderer.material.mainTexture.name.Equals(textureConfig[0], StringComparison.OrdinalIgnoreCase))
-                        {
-                            WWW www = new WWW(("file:///" + ModloaderMod.Instance.Modpath + "/" + textureConfig[1]).Replace(" ", "%20").Replace("\\", "/"));
-                            www.LoadImageIntoTexture(meshRenderer.material.mainTexture as Texture2D);
-                        }
+                        ConfigHolder.Instance.textureReplacements.GetValueSafe(meshRenderer.material.mainTexture.name).LoadImageIntoTexture(meshRenderer.material.mainTexture as Texture2D);
                     }
                 }
             }
@@ -34,75 +75,20 @@ namespace Texture_Replacer
         }
     }
 
-    [HarmonyPatch(typeof(UnityEngine.Object))]
-    [HarmonyPatch("Instantiate")]
-    [HarmonyPatch(new Type[] { typeof(MonoBehaviour) })]
+    [HarmonyPatch(typeof(Image))]
+    [HarmonyPatch("sprite", PropertyMethod.Getter)]
     class PatchOthers
     {
-        static MonoBehaviour Postfix(MonoBehaviour __result)
+        static void Postfix(ref Sprite __result)
         {
-            string[] textureConfigurations = File.ReadAllLines(ModloaderMod.Instance.Modpath + "/Sprite Texture Replacer.conf");
-
-            Image[] images = __result.gameObject.GetComponentsInChildren<Image>(true);
-            foreach (Image image in images)
+            if (__result != null && __result.texture != null)
             {
-                if (image.sprite != null && image.sprite.texture != null)
+                if (ConfigHolder.Instance.imageReplacements.ContainsKey(__result.texture.name))
                 {
-                    File.AppendAllText(ModloaderMod.Instance.Modpath + "/test.log", image.sprite.texture.name + "\n");
-                    foreach (string textureConfigLine in textureConfigurations)
-                    {
-                        string[] textureConfig = textureConfigLine.Split('|');
-                        if (image.sprite.texture.name.Equals(textureConfig[0], StringComparison.OrdinalIgnoreCase))
-                        {
-                            WWW www = new WWW(("file:///" + ModloaderMod.Instance.Modpath + "/" + textureConfig[1]).Replace(" ", "%20").Replace("\\", "/"));
-                            www.LoadImageIntoTexture(image.sprite.texture);
-                        }
-                    }
+                    ConfigHolder.Instance.imageReplacements.GetValueSafe(__result.texture.name).LoadImageIntoTexture(__result.texture as Texture2D);
                 }
             }
-            return __result;
         }
     }
-
-    //[HarmonyPatch(typeof(SaveLoadSystem))]
-    //[HarmonyPatch("LoadSaveGame")]
-    //class PatchOthers
-    //{
-    //    static void Postfix()
-    //    {
-    //        string[] textureConfigurations = File.ReadAllLines(PCBSModloader.ModLoader.PatchesPath + "/Texture Replacer/Sprite Texture Replacer.conf");
-    //        GameObject[] rootGameObjects = SceneManager.GetActiveScene().GetRootGameObjects();
-    //        foreach (GameObject go in rootGameObjects)
-    //        {
-    //            Texture2D[] test1 = go.GetComponentsInChildren<Texture2D>(true);
-    //            foreach (Texture2D texture in test1)
-    //            {
-    //                if (texture != null)
-    //                {
-    //                    File.AppendAllText(PCBSModloader.ModLoader.PatchesPath + "/Texture Replacer/testtex.log", texture.name + "\n");
-    //                }
-    //            }
-    //            Image[] images = go.GetComponentsInChildren<Image>(true);
-    //            foreach (Image image in images)
-    //            {
-    //                if (image.sprite != null && image.sprite.texture != null)
-    //                {
-    //                    File.AppendAllText(PCBSModloader.ModLoader.PatchesPath + "/Texture Replacer/test.log", image.sprite.texture.name + "\n");
-    //                    foreach (string textureConfigLine in textureConfigurations)
-    //                    {
-    //                        string[] textureConfig = textureConfigLine.Split('|');
-    //                        if (image.sprite.texture.name.Equals(textureConfig[0], StringComparison.OrdinalIgnoreCase))
-    //                        {
-    //                            WWW www = new WWW(("file:///" + PCBSModloader.ModLoader.PatchesPath + "/Texture Replacer/" + textureConfig[1]).Replace(" ", "%20").Replace("\\", "/"));
-    //                            www.LoadImageIntoTexture(image.sprite.texture);
-    //                        }
-    //                    }
-    //                }
-    //            }
-    //        }
-    //    }
-    //}
-
-
 
 }
--- Texture Replacer/readme.txt
+++ Texture Replacer/readme.txt
@@ -1,6 +1,6 @@
 ﻿## Texture Replacer for PC Building Simulator
 This is a mod that allows you to overwrite pretty much any texture in the game with your own. As an example it overwrites a few of the desktop icons with Windows 98 versions and it changes the Ryzen 7 1700 texture to a 486DX. 
-See the Quick Start section below on how to make your texture replacements with this!
+See the Quick Start section below on how to make your own texture replacements with this!
 
 ## Requirements
 Requires FusioN.'s modloader for PCBS to be installed ( https://github.com/fusiion/PCBSModloader/releases )
@@ -21,9 +21,9 @@ The Harmony C# patching library ( https://github.com/pardeike/Harmony )
 2. Search for the Texture you want to change
 	2a. Select "Show Type" -> "Texture2D"
 	2b. Go to the "Asset List" tab and search for your texture, examples to search for are "Ryzen 5" or "icon_"
-	2c. Note down the texture name
+	2c. Note down the texture name, be careful as this is case sensitive!
 3. Copy the new image you want to use into this folder, PNG or JPG works, for best results make it the same size as the original texture (AssetStudio shows width and height in the preview window)
 4. Now depending on if the texture is for a part or for an icon/poster or other graphics in the game:
 	4a. Part texture: Open "Parts Texture Replacer.conf" with a text editor and add a line <texture name>|<your new texture file> so as an example it will look like "Ryzen 5 Quad Core 1400|newCpu.png"
-	4b. Icon/poster/image: Open "Sprite Texture Replacer.conf" with a text editor and add a line <texture name>|<your new texture file> so as an example it will look like "icon_email|myMail.jpg"
+	4b. Icon/poster/image: Open "Image Replacer.conf" with a text editor and add a line <texture name>|<your new texture file> so as an example it will look like "icon_email|myMail.jpg"
 5. Start the game and try it out!
