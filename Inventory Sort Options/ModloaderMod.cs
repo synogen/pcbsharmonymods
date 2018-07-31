e57ee895ab534bf60c99a457aca9fec56baea27d
synogen <synogen@freenet.de>
2018-08-01 00:45:59 +0200

    basic version of new Inventory Sort Options mod added, only reverses item order right now

---
+++ Inventory Sort Options/ModloaderMod.cs
@@ -0,0 +1,24 @@
+﻿using PCBSModloader;
+
+namespace Inventory_Sort_Options
+{
+    class ModloaderMod : Mod
+    {
+        public override string ID { get { return "Inventory Sort Options"; } }
+
+        public override string Version { get { return "1"; } }
+
+        public override string Author { get { return "synogen"; } }
+
+        private static ModloaderMod singletonInstance;
+
+        public static ModloaderMod Instance {
+            get
+            {
+                singletonInstance = singletonInstance != null ? singletonInstance : new ModloaderMod();
+                return singletonInstance;
+            }
+        }
+
+    }
+}
---
+++ Inventory Sort Options/Patch.cs
@@ -0,0 +1,18 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Inventory_Sort_Options
+{
+    [HarmonyPatch(typeof(Inventory))]
+    [HarmonyPatch("UpdateInventory")]
+    class Patch
+    {
+        static void Prefix(ref List<PartInstance> ___itemsDisplayedInInventory)
+        {
+            ___itemsDisplayedInInventory.Reverse();
+        }
+    }
+}
