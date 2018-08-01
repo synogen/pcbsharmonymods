92b8065f9469bf0ca493cf2a6410ecf07c6d5844
synogen <synogen@freenet.de>
2018-08-01 23:44:33 +0200

    added really basic mod for stepping the OC sliders by 1 each for maximum precision when using arrow keys to change sliders

---
+++ Slider Precision Step/ModloaderMod.cs
@@ -0,0 +1,24 @@
+﻿using PCBSModloader;
+
+namespace Slider_Precision_Step
+{
+    class ModloaderMod : Mod
+    {
+        public override string ID { get { return "Slider Precision Step"; } }
+
+        public override string Version { get { return "1"; } }
+
+        public override string Author { get { return "synogen"; } }
+
+        private static ModloaderMod singletonInstance;
+
+        public static ModloaderMod Instance {
+            get
+            {
+                singletonInstance = singletonInstance != null ? singletonInstance : new ModloaderMod();
+                return singletonInstance;
+            }
+        }
+
+    }
+}
---
+++ Slider Precision Step/Patch.cs
@@ -0,0 +1,15 @@
+﻿using Harmony;
+using UnityEngine.UI;
+
+namespace Slider_Precision_Step
+{
+    [HarmonyPatch(typeof(Slider))]
+    [HarmonyPatch("stepSize", PropertyMethod.Getter)]
+    class PatchLevelLoad
+    {
+        static float Postfix(float __result)
+        {
+            return 1;
+        }
+    }
+}
